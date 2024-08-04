0fb7ecd6535bf949db8694b675a1251eb17297e1
pexlor <1018734423@qq.com>
2024-08-04 20:19:52 +0800

    add test

--- personal/learn.cpp
+++ personal/learn.cpp
@@ -1,8 +1,43 @@
 #include <atomic>
+#include <thread>
+#include <iostream>
 using namespace std;
 
+atomic_uint64_t i(1);
+
+void Test1(){
+    while(1){
+        if(i.load()==1){
+            cout << "1 ";
+            i.store(2);
+        }
+    }
+}
+
+void Test2(){
+    while(1){
+        if(i.load()==2){
+            cout << "2 ";
+            i.store(3);
+        }
+    }
+}
+
+void Test3(){
+    while(1){
+        if(i.load()==3){
+            cout << "3\n";
+            i.store(1);
+        }
+    }
+}
+
 int main(){
-    atomic_bool a;
-    atomic<bool> b;
-    a.load();
+    thread t3(Test3);
+    thread t1(Test1);
+    thread t2(Test2);
+    t1.join();
+    t2.join();
+    t3.join();
+    return 0;
 }
